ac830ad3ae53dcc43211d7809be8c5a4ad0cfd4d
HunterHillegas <hunter@lastonepicked.com>
2011-05-25 10:05:56 -0700

    window is now drag target

---
+++ BetaBuilderWindow.h
@@ -0,0 +1,17 @@
+//
+//  BetaBuilderWindow.h
+//  BetaBuilder
+//
+//  Created by Hunter Hillegas on 5/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface BetaBuilderWindow : NSWindow {
+@private
+    
+}
+
+@end
---
+++ BetaBuilderWindow.m
@@ -0,0 +1,29 @@
+//
+//  BetaBuilderWindow.m
+//  BetaBuilder
+//
+//  Created by Hunter Hillegas on 5/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "BetaBuilderWindow.h"
+
+
+@implementation BetaBuilderWindow
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
